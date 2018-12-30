27ee76de9853f3da131dc68a0945f0a82d8f0cac
mjvandas <malakivandas@gmail.com>
2018-12-29 22:26:03 -0800

    Update Add_Names.py
    Added input variables

--- Add_Names.py
+++ Add_Names.py
@@ -1,2 +1,8 @@
-def main()
+def main
+	
+	#Input
+	first_name = input("What is your first name?")
+	last_name = input("What is your last name?")
 
+	print("First Name:"), first_name
+	print("Last Name:"), last_name
