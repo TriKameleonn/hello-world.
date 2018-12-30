0e4f1c2f20634bcd0a7297b6b27eba4fe8825b3b
mbvandas <46204629+TriKameleonn@users.noreply.github.com>
2018-12-29 22:32:46 -0800

    Input Output Mal's Project

--- Coding For Beginers.py
+++ Coding For Beginers.py
@@ -1,8 +1,2 @@
-var=8
-print(var)
-var=3.142
-print(var)
-var="Coding for Beginers in easy steps"
-print(var)
-var=True
-print(var)
+running=True
+print("Run State:" ,running)
---
+++ Input Output Matthew Practice.py
@@ -0,0 +1,4 @@
+name=input("please enter your name:")
+print("hello",name)
+print("Welcome to Coding for Beginners!")
+print("remember to have fun",name) 
